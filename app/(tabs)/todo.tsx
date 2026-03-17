908e93744844da171fdb4f121381e4aed1e49abc
Bereket-Ketema <bereketketema866@gmail.com>
2026-03-17 07:46:34 +0300

    todo list app have been created

---
+++ app/(tabs)/todo.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default function Todo() {
+    return (
+      <View>
+        <Text>todo</Text>
+      </View>
+    )
+}
