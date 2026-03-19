bd3d632cdcf3e7697986a0cace13ecf7a816b246
Bereket-Ketema <bereketketema866@gmail.com>
2026-03-19 04:57:14 +0300

    home page have been set

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import Greeting from "@/components/Greeting";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+  function goToDetails() {
+    router.push({
+      pathname: "../details",
+      params: {
+        name: "Beki",
+        age: "20",
+      },
+    });
+  }
   return (
     <View>
-      <Text>Name</Text>
       <Greeting name="Bereket" role='Mobile app Developer'/>
+      <Text onPress={() => router.push('../add')} style={{color: 'blue', marginTop: 20}}>Go to Add Task</Text>
+      <Pressable onPress={goToDetails}>
+        <Text>Go to Details</Text>
+      </Pressable>
     </View>
   );
 }
