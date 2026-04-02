4c96dee2a335d27257633da8e2e3dc1d46b0f8d3
Bereket-Ketema <bereketketema866@gmail.com>
2026-04-02 06:32:41 +0300

    the stack have been wrapped by task provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
 import { useColorScheme } from "@/app-example/hooks/use-color-scheme";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { TaskProvider } from "../context/TaskContext";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return(
+    
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <TaskProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
+      </TaskProvider>
     </ThemeProvider>
   );
 }
